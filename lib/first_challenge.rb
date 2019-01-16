d93eb9d1ccc0ea56ede8cbcf780eb40d6cf54e5a
Luis Zabala <luis.zabala076@gmail.com>
2019-01-16 15:53:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,40 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {
+         :name => "Lord Montague",
+         :age => "53"
+       },
+       :matriarch => {
+         :name => "Lady Montague",
+         :age => "54"
+       },
+       :hero => {
+         :name => "Romeo",
+         :age => "15"
+       },
+       :hero_friends => [
+         {
+           :name => "Benvoilo",
+           :age => "17",
+           :attitude => "worried"
+         },
+         {
+           :name => "Mercutio",
+           :age => "18",
+           :attitude => "hot-headed"
+         }
+         ]
+   },
+   :capulete => {
+     :patriarch => {
+       :name => "Lord Capulete",
+       :age => "50"
+     },
+     :matriarch => "Lady Capulete",
+     :age => "51"
+     
+     },
+     }
+   }
 end
